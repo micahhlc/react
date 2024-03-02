b1e7fdd6795cd23524facc932ec2e959b20de6b8
Micah <micahhlc@gmail.com>
2024-03-02 19:59:56 +0900

    update emoji

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -1,27 +1,29 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import emojipedia from "./emojipedia";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div>
-    <h1>
-      <span>emojipedia</span>
-    </h1>
-    <dl className="dictionary">
-      {emojipedia.map((e) => {
-        return (
-          <Dictionary
-            key={e.id}
-            name={e.name}
-            emoji={e.emoji}
-            meaning={e.meaning}
-          />
-        );
-      })}
-    </dl>
-  </div>
+    <>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <div className="container">
+        <dl className="dictionary">
+          {emojipedia.map((e) => {
+            return (
+              <Dictionary
+                key={e.id}
+                name={e.name}
+                emoji={e.emoji}
+                meaning={e.meaning}
+              />
+            );
+          })}
+        </dl>
+      </div>
+    </>
   );
 }
 
--- emoji/src/Dictionary.jsx
+++ emoji/src/Dictionary.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { useState } from 'react';
+import "./Dictionary.css";
+
+
+function LikeButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <>
+    <div className="likebutton">
+      <button className="pushable" onClick={handleClick}>
+        <span className="shadow"></span>
+        <span className="edge"></span>
+        <span className="front">Liked {count} times</span>
+      </button>
+    </div>
+    </>
+    
+  );
+}
 
 function Dictionary(props) {
+  
   return (
     <>
       <div className="term">
@@ -8,9 +32,12 @@ function Dictionary(props) {
           <span className="emoji" role="img" aria-label={props.name}>
             {props.emoji}
           </span>
+        </dt>
+        <dt>
           <span>{props.name}</span>
         </dt>
         <dd>{props.meaning}</dd>
+        <LikeButton />
       </div>
     </>
   );
